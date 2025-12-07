5113867f48050ff1f44ba4d2ee8a136704b2311b
Yann Mantelli <yann.mantelli@viacesi;fr>
2025-12-07 22:22:08 +0100

    feat : maj rencontre

--- src/component/documentation/bestiaire/bestiaire.component.ts
+++ src/component/documentation/bestiaire/bestiaire.component.ts
@@ -27,7 +27,7 @@ private buildCardList():Array<Card>{
     {
       image:"assets/img/card/bestiaire/desert.png",
       titre:"Désert",
-      url:"assets/img/pdfs/bestiaire/rencontres-desert-naheulbeuk-jdr.pdf"
+      url:"assets/img/pdfs/bestiaire/rencontre-desert.PNG"
     },
     {
       image:"assets/img/card/bestiaire/est.png",
@@ -36,53 +36,28 @@ private buildCardList():Array<Card>{
     },
     {
       image:"assets/img/card/bestiaire/foret1.png",
-      titre:"Fôret bas level",
-      url:"assets/img/pdfs/bestiaire/rencontres-foret1-naheulbeuk-jdr.pdf"
-    },
-    {
-      image:"assets/img/card/bestiaire/foret2.png",
       titre:"Fôret",
-      url:"assets/img/pdfs/bestiaire/rencontres-foret2-naheulbeuk-jdr.pdf"
-    },
-    {
-      image:"assets/img/card/bestiaire/givre.png",
-      titre:"Givre",
-      url:"assets/img/pdfs/bestiaire/bestiaire-confinsdugivre-naheulbeuk-jdr.pdf"
-    },
-    {
-      image:"assets/img/card/bestiaire/grotte.png",
-      titre:"Grotte",
-      url:"assets/img/pdfs/bestiaire/Rencontres-grottes-naheulbeuk.pdf"
+      url:"assets/img/pdfs/bestiaire/rencontre-foret.PNG"
     },
     {
       image:"assets/img/card/bestiaire/jungle.png",
       titre:"Jungle",
-      url:"assets/img/pdfs/bestiaire/jungle.PNG"
+      url:"assets/img/pdfs/bestiaire/rencontre-jungle.PNG"
     },
     {
       image:"assets/img/card/bestiaire/mer.jpg",
       titre:"Mer",
-      url:"assets/img/pdfs/bestiaire/mer.PNG"
+      url:"assets/img/pdfs/bestiaire/rencontre-mer.PNG"
     },
     {
       image:"assets/img/card/bestiaire/montagne.png",
       titre:"Montagne",
-      url:"assets/img/pdfs/bestiaire/rencontres-montagne-naheulbeuk-jdr.pdf"
-    },
-    {
-      image:"assets/img/card/bestiaire/souterrain.png",
-      titre:"Souterrain",
-      url:"assets/img/pdfs/bestiaire/rencontres-souterrains-naheulbeuk-jdr.pdf"
+      url:"assets/img/pdfs/bestiaire/rencontre-montagne.PNG"
     },
     {
       image:"assets/img/card/bestiaire/terre-sauvage.png",
       titre:"Terre Sauvage",
-      url:"assets/img/pdfs/bestiaire/rencontres-terresauvage2-naheulbeuk-jdr.pdf"
-    },
-    {
-      image:"assets/img/card/bestiaire/urbain.png",
-      titre:"Urbain",
-      url:"assets/img/pdfs/bestiaire/rencontres-urbaines2-naheulbeuk-jdr.pdf"
+      url:"assets/img/pdfs/bestiaire/rencontre-plaine.PNG"
     },
   ]
 
