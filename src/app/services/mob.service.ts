a8463877ac817224244b58f3a976291cea9d3587
Yann Mantelli <yann.mantelli@viacesi;fr>
2026-07-26 22:45:13 +0200

    feat : rework carte

--- src/app/services/mob.service.ts
+++ src/app/services/mob.service.ts
@@ -1,63 +1,69 @@
 import { Injectable } from '@angular/core';
-import { collection, doc, DocumentData, Firestore, getDocs,  query, setDoc } from '@angular/fire/firestore';
+import {
+  collection,
+  doc,
+  DocumentData,
+  Firestore,
+  getDocs,
+  query,
+  setDoc,
+} from '@angular/fire/firestore';
 import { Mob } from '../../component/model/ennemi';
 import { StorageKeys, StorageService } from './storage.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MobsService {
-
   constructor(
     public firestore: Firestore,
-    public storage : StorageService
-  ) { }
+    public storage: StorageService,
+  ) {}
 
-
-  async getAll(){
-
-    if(!this.storage.get(StorageKeys.MOBS)){
-      const mobs = (await getDocs(query(collection(this.firestore,'mobs')))).docs.map((entries) => entries.data());
-      this.storage.set<DocumentData[]>(StorageKeys.MOBS,mobs);
+  async getAll() {
+    if (!this.storage.get(StorageKeys.MOBS)) {
+      const mobs = (
+        await getDocs(query(collection(this.firestore, 'mobs')))
+      ).docs.map((entries) => entries.data());
+      this.storage.set<DocumentData[]>(StorageKeys.MOBS, mobs);
     }
-        
+
     return (this.storage.get<DocumentData[]>(StorageKeys.MOBS) ?? []) as Mob[];
   }
 
-  async getByZone(zone:string){
-    const mobs = (await this.getAll())
-      .filter(x=>x.zone == zone);
+  async getByZone(zone: string) {
+    const mobs = (await this.getAll()).filter((x) => x.zone == zone);
 
     return mobs;
   }
 
-  async insert(mob:Mob){
+  async insert(mob: Mob) {
     let code = mob.libelle
-      .replaceAll(" ","-")
-      .replaceAll("'","-")
+      .replaceAll(' ', '-')
+      .replaceAll("'", '-')
       .toLowerCase()
-      .normalize("NFD")
-      .replace(/\p{Diacritic}/gu, "");
-      
-    await setDoc(doc(this.firestore, "mobs",code ), {
-      code: code ,
+      .normalize('NFD')
+      .replace(/\p{Diacritic}/gu, '');
+
+    await setDoc(doc(this.firestore, 'mobs', code), {
+      code: code,
       libelle: mob.libelle,
-      zone:'autre',
-      attaque:mob.attaque,
-      parade:mob.parade,
-      vie:mob.vie,
-      informations:mob.informations,
-      degats:mob.degats,
-      armure:mob.armure,
-      experience:mob.experience,
+      zone: 'autre',
+      attaque: mob.attaque,
+      parade: mob.parade,
+      vie: mob.vie,
+      informations: mob.informations,
+      degats: mob.degats,
+      armure: mob.armure,
+      armureMagique: mob.armureMagique,
+      experience: mob.experience,
     });
 
     await this.storage.addElementInStorageGroup(StorageKeys.MOBS, mob);
   }
 
-  async bulkInsert(){
-
-    let mobs : Mob[] = [];
+  async bulkInsert() {
+    let mobs: Mob[] = [];
     mobs = [
       // ...EnnemiHelper.getMobEst(),
       // ...EnnemiHelper.getMobDesert(),
@@ -67,24 +73,19 @@ export class MobsService {
       // ...EnnemiHelper.getMobTerreSauvage(),
     ];
 
-    mobs.forEach(async (mob) =>{
-      await setDoc(doc(this.firestore, "mobs",mob.code ), {
-              code: mob.code ,
-              libelle: mob.libelle,
-              zone:mob.zone,
-              attaque:mob.attaque,
-              parade:mob.parade,
-              vie:mob.vie,
-              informations:mob.informations,
-              degats:mob.degats,
-              armure:mob.armure,
-              experience:mob.experience,
-            });
-      
-    })
-
+    mobs.forEach(async (mob) => {
+      await setDoc(doc(this.firestore, 'mobs', mob.code), {
+        code: mob.code,
+        libelle: mob.libelle,
+        zone: mob.zone,
+        attaque: mob.attaque,
+        parade: mob.parade,
+        vie: mob.vie,
+        informations: mob.informations,
+        degats: mob.degats,
+        armure: mob.armure,
+        experience: mob.experience,
+      });
+    });
   }
-
-  
-
 }
