75c435a54c2ca45300d9fa45b3b014e95ef2fc11
Yann Mantelli <yann.mantelli@viacesi;fr>
2026-08-13 20:00:14 +0200

    feat : grotte bestiaire

--- src/component/documentation/bestiaire/bestiaire.component.ts
+++ src/component/documentation/bestiaire/bestiaire.component.ts
@@ -7,60 +7,65 @@ import { CardListComponent } from '../../common/card-list/card-list.component';
   selector: 'app-bestiaire',
   templateUrl: './bestiaire.component.html',
   styleUrl: './bestiaire.component.css',
-  standalone:true,
-  imports:[CommonModule,CardListComponent]
+  standalone: true,
+  imports: [CommonModule, CardListComponent],
 })
 export class BestiaireComponent {
   title = 'nhbk';
   cards: Array<Card>;
 
-  constructor(){
+  constructor() {
     this.cards = this.buildCardList();
   }
 
-  public openPdf(url:string):void{
-    window.open(url,'_blank')
+  public openPdf(url: string): void {
+    window.open(url, '_blank');
   }
-  
-private buildCardList():Array<Card>{
-  const list : Array<Card> = [
-    {
-      image:"assets/img/card/bestiaire/desert.png",
-      titre:"Désert",
-      url:"assets/img/pdfs/bestiaire/rencontre-desert.PNG"
-    },
-    {
-      image:"assets/img/card/bestiaire/est.png",
-      titre:"Est",
-      url:"assets/img/pdfs/bestiaire/est.PNG"
-    },
-    {
-      image:"assets/img/card/bestiaire/foret1.png",
-      titre:"Forêt",
-      url:"assets/img/pdfs/bestiaire/rencontre-foret.PNG"
-    },
-    {
-      image:"assets/img/card/bestiaire/jungle.png",
-      titre:"Jungle",
-      url:"assets/img/pdfs/bestiaire/rencontre-jungle.PNG"
-    },
-    {
-      image:"assets/img/card/bestiaire/mer.jpg",
-      titre:"Mer",
-      url:"assets/img/pdfs/bestiaire/rencontre-mer.PNG"
-    },
-    {
-      image:"assets/img/card/bestiaire/montagne.png",
-      titre:"Montagne",
-      url:"assets/img/pdfs/bestiaire/rencontre-montagne.PNG"
-    },
-    {
-      image:"assets/img/card/bestiaire/terre-sauvage.png",
-      titre:"Terre Sauvage",
-      url:"assets/img/pdfs/bestiaire/rencontre-plaine.PNG"
-    },
-  ]
 
-  return list;
-}
+  private buildCardList(): Array<Card> {
+    const list: Array<Card> = [
+      {
+        image: 'assets/img/card/bestiaire/desert.png',
+        titre: 'Désert',
+        url: 'assets/img/pdfs/bestiaire/rencontre-desert.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/est.png',
+        titre: 'Est',
+        url: 'assets/img/pdfs/bestiaire/est.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/foret1.png',
+        titre: 'Forêt',
+        url: 'assets/img/pdfs/bestiaire/rencontre-foret.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/jungle.png',
+        titre: 'Jungle',
+        url: 'assets/img/pdfs/bestiaire/rencontre-jungle.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/mer.jpg',
+        titre: 'Mer',
+        url: 'assets/img/pdfs/bestiaire/rencontre-mer.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/montagne.png',
+        titre: 'Montagne',
+        url: 'assets/img/pdfs/bestiaire/rencontre-montagne.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/terre-sauvage.png',
+        titre: 'Terre Sauvage',
+        url: 'assets/img/pdfs/bestiaire/rencontre-plaine.PNG',
+      },
+      {
+        image: 'assets/img/card/bestiaire/rencontre-sousterrain.png',
+        titre: 'Sous terre',
+        url: 'assets/img/pdfs/bestiaire/rencontre-sousterrain.PNG',
+      },
+    ];
+
+    return list;
+  }
 }
